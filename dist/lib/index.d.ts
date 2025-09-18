e9e1d208ac83239eb2354e57991f21bf80dbc1cf
shafat420 <shafat420@gmail.com>
2025-09-18 12:08:58 +0600

    fix

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -9,9 +9,73 @@ export type ChapterImagesResult = {
     jpegUrls: string[];
 };
 export declare function fetchChapterImages(path: string): Promise<ChapterImagesResult>;
+export type LatestReleasesParams = {
+    genre?: string;
+    genres_excs?: string[];
+    genres_incs?: string[];
+    init?: number;
+    multi?: boolean;
+    page?: number;
+    size?: number;
+};
+export declare function getLatestReleases(params?: LatestReleasesParams): Promise<any>;
+export type MemberUploadsParams = {
+    init?: number;
+    page?: number;
+    size?: number;
+};
+export declare function getMemberUploads(params?: MemberUploadsParams): Promise<any>;
+export declare function getRandomMangas(amount?: number): Promise<any>;
+export declare function getYWeekList(): Promise<any>;
+export declare function getMplistsWeekly(yweek: number, limit?: number): Promise<any>;
+export declare function getMplistsNewlyAdded(params?: {
+    comicId?: number | null;
+    init?: number;
+    page?: number;
+    size?: number;
+}): Promise<any>;
+export declare function getMplistsMostLikes(params?: {
+    comicId?: number | null;
+    init?: number;
+    page?: number;
+    size?: number;
+}): Promise<any>;
+export declare function searchComics(params?: {
+    page?: number;
+    size?: number;
+    sortby?: string;
+    word?: string;
+}): Promise<any>;
+export type InfoType = {
+    img: string | null;
+    title: string | null;
+    otherInfo: string[];
+    search: string[];
+    genres: string[];
+    originalPulication: string | null;
+    mParkUploadStatus: string | null;
+    description: string | null;
+};
+export declare function getTitleInfo(id: string): Promise<InfoType>;
+export type ChapterType = {
+    id: string | null;
+    chapter: string | null;
+    uploaded: string | null;
+};
+export declare function getTitleChapters(id: string): Promise<ChapterType[]>;
 declare const _default: {
     getPopularUpdates: typeof getPopularUpdates;
     fetchChapterImages: typeof fetchChapterImages;
+    getLatestReleases: typeof getLatestReleases;
+    getMemberUploads: typeof getMemberUploads;
+    getRandomMangas: typeof getRandomMangas;
+    getYWeekList: typeof getYWeekList;
+    getMplistsWeekly: typeof getMplistsWeekly;
+    getMplistsNewlyAdded: typeof getMplistsNewlyAdded;
+    getMplistsMostLikes: typeof getMplistsMostLikes;
+    searchComics: typeof searchComics;
+    getTitleInfo: typeof getTitleInfo;
+    getTitleChapters: typeof getTitleChapters;
 };
 export default _default;
 //# sourceMappingURL=index.d.ts.map
--- dist/lib/index.js
+++ dist/lib/index.js
@@ -35,6 +35,16 @@ var __importStar = (this && this.__importStar) || (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPopularUpdates = getPopularUpdates;
 exports.fetchChapterImages = fetchChapterImages;
+exports.getLatestReleases = getLatestReleases;
+exports.getMemberUploads = getMemberUploads;
+exports.getRandomMangas = getRandomMangas;
+exports.getYWeekList = getYWeekList;
+exports.getMplistsWeekly = getMplistsWeekly;
+exports.getMplistsNewlyAdded = getMplistsNewlyAdded;
+exports.getMplistsMostLikes = getMplistsMostLikes;
+exports.searchComics = searchComics;
+exports.getTitleInfo = getTitleInfo;
+exports.getTitleChapters = getTitleChapters;
 const cheerio = __importStar(require("cheerio"));
 const client_1 = require("./client");
 // High-level: fetch + return MangaPark popular updates (GraphQL)
@@ -117,7 +127,197 @@ async function fetchChapterImages(path) {
     const jpegUrls = object.filter((item) => typeof item === 'string' && item.match(/\.(jpe?g)(\?.*)?$/i));
     return { totalImg: jpegUrls.length, jpegUrls };
 }
+async function getLatestReleases(params = {}) {
+    const { genre = '', genres_excs = [], genres_incs = [], init = 18, multi = false, page = 1, size = 36, } = params;
+    const query = `
+  query get_latestReleases($select: LatestReleases_Select) {
+    get_latestReleases(select: $select) {
+      paging { total pages page init size skip limit prev next }
+      items {
+        id
+        data {
+          id dbStatus name origLang tranLang urlPath urlCover600 urlCoverOri genres sfw_result is_hot is_new score_val follows reviews comments_total
+          last_chapterNodes(amount: 1) { id data { id dateCreate dbStatus isFinal dname urlPath is_new userId userNode { id data { id name uniq avatarUrl urlPath } } } }
+        }
+        sser_follow
+        sser_lastReadChap { date chapterNode { id data { id dbStatus isFinal dname urlPath is_new userId userNode { id data { id name uniq avatarUrl urlPath } } } } }
+      }
+    }
+  }`;
+    const response = await client_1.client.post('/apo/', {
+        query,
+        variables: {
+            select: {
+                genre,
+                genres_excs,
+                genres_incs,
+                init: Number(init),
+                multi: Boolean(multi),
+                page: Number(page),
+                size: Number(size),
+                where: 'release',
+            },
+        },
+    });
+    return response.data?.data?.get_latestReleases ?? null;
+}
+async function getMemberUploads(params = {}) {
+    const { init = 6, page = 1, size = 6 } = params;
+    const query = `
+  query get_latestReleases($select: LatestReleases_Select) {
+    get_latestReleases(select: $select) {
+      paging { total pages page init size skip limit prev next }
+      items {
+        id
+        data {
+          id dbStatus name origLang tranLang urlPath urlCover600 urlCoverOri genres sfw_result is_hot is_new score_val follows reviews comments_total
+          last_userChapterNodes(amount: 1) { id data { id dateCreate dbStatus isFinal dname urlPath is_new userId userNode { id data { id name uniq avatarUrl urlPath } } } }
+        }
+        sser_follow
+        sser_lastReadChap { date chapterNode { id data { id dbStatus isFinal dname urlPath is_new userId userNode { id data { id name uniq avatarUrl urlPath } } } } }
+      }
+    }
+  }`;
+    const response = await client_1.client.post('/apo/', {
+        query,
+        variables: { select: { init: Number(init), page: Number(page), size: Number(size), where: 'uploads' } },
+    });
+    return response.data?.data?.get_latestReleases ?? null;
+}
+// Random Mangas
+async function getRandomMangas(amount = 12) {
+    const query = `query get_random_comicList($amount: Int) { get_random_comicList(amount: $amount) { id data { id name urlCover600 urlPath sfw_result } } }`;
+    const response = await client_1.client.post('/apo/', { query, variables: { amount: Number(amount) } });
+    return response.data?.data?.get_random_comicList ?? null;
+}
+// yweek list
+async function getYWeekList() {
+    const query = `query get_mylist_yweekList { get_mylist_yweekList }`;
+    const response = await client_1.client.post('/apo/', { query });
+    return response.data?.data?.get_mylist_yweekList ?? null;
+}
+// mplists weekly by yweek
+async function getMplistsWeekly(yweek, limit = 12) {
+    if (yweek == null)
+        throw new Error('yweek is required');
+    const query = `
+  query get_mylist_weeklyLists($select: Mylist_WeeklyLists_Select) {
+    get_mylist_weeklyLists(select: $select) {
+      start yweek
+      items {
+        id iid hash
+        data {
+          id hash name urlPath comics likes comments_total
+          userId userNode { id data { id name uniq avatarUrl urlPath } }
+          comicNodes(amount: 4) { id data { id name urlPath urlCover600 urlCoverOri } }
+        }
+      }
+    }
+  }`;
+    const response = await client_1.client.post('/apo/', { query, variables: { select: { limit: Number(limit), yweek: Number(yweek) } } });
+    return response.data?.data?.get_mylist_weeklyLists ?? null;
+}
+// mplists newly added
+async function getMplistsNewlyAdded(params = {}) {
+    const { comicId = null, init = 6, page = 1, size = 12 } = params;
+    const query = `
+  query get_mylist_SortedList($select: MylistList_Select) {
+    get_mylist_SortedList(select: $select) {
+      paging { total pages page init size skip limit prev next }
+      items {
+        id hash
+        data {
+          id hash name urlPath comics likes comments_total
+          userId userNode { id data { id name uniq avatarUrl urlPath } }
+          comicNodes(amount: 4) { id data { id name urlPath urlCover600 urlCoverOri } }
+        }
+      }
+    }
+  }`;
+    const response = await client_1.client.post('/apo/', { query, variables: { select: { comicId: comicId == null ? null : Number(comicId), init: Number(init), page: Number(page), size: Number(size), sortby: 'create' } } });
+    return response.data?.data?.get_mylist_SortedList ?? null;
+}
+// mplists most likes
+async function getMplistsMostLikes(params = {}) {
+    const { comicId = null, init = 6, page = 1, size = 12 } = params;
+    const query = `
+  query get_mylist_SortedList($select: MylistList_Select) {
+    get_mylist_SortedList(select: $select) {
+      paging { total pages page init size skip limit prev next }
+      items {
+        id hash
+        data {
+          id hash name urlPath comics likes comments_total
+          userId userNode { id data { id name uniq avatarUrl urlPath } }
+          comicNodes(amount: 4) { id data { id name urlPath urlCover600 urlCoverOri } }
+        }
+      }
+    }
+  }`;
+    const response = await client_1.client.post('/apo/', { query, variables: { select: { comicId: comicId == null ? null : Number(comicId), init: Number(init), page: Number(page), size: Number(size), sortby: 'likes' } } });
+    return response.data?.data?.get_mylist_SortedList ?? null;
+}
+// search
+async function searchComics(params = {}) {
+    const { page = 1, size = 36, sortby = 'field_score', word = 'one piece' } = params;
+    const query = `
+  query get_searchComic($select: SearchComic_Select) {
+    get_searchComic(select: $select) {
+      reqPage reqSize reqSort reqWord newPage
+      paging { total pages page init size skip limit prev next }
+      items {
+        id
+        data { id dbStatus name origLang tranLang urlPath urlCover600 urlCoverOri genres altNames authors artists is_hot is_new sfw_result score_val follows reviews comments_total
+          max_chapterNode { id data { id dateCreate dbStatus isFinal sfw_result dname urlPath is_new userId userNode { id data { id name uniq avatarUrl urlPath } } } }
+        }
+        sser_follow
+        sser_lastReadChap { date chapterNode { id data { id dbStatus isFinal sfw_result dname urlPath is_new userId userNode { id data { id name uniq avatarUrl urlPath } } } } }
+      }
+    }
+  }`;
+    const response = await client_1.client.post('/apo/', { query, variables: { select: { page: Number(page), size: Number(size), sortby, word } } });
+    return response.data?.data?.get_searchComic ?? null;
+}
+async function getTitleInfo(id) {
+    if (!id)
+        throw new Error('id is required');
+    const response = await client_1.client.get(`/title/${id}`);
+    const $ = cheerio.load(response.data);
+    const data = { img: null, title: null, otherInfo: [], search: [], genres: [], originalPulication: null, mParkUploadStatus: null, description: null };
+    const selector = $('main div:nth-child(1)');
+    data.img = $(selector).find('.flex img').attr('src') || null;
+    data.title = $(selector).find('.flex > div:nth-child(2) > h3 a').text().trim() || null;
+    $(selector).find('.flex div:nth-child(2) > .mt-1.text-xs span').each(function () { const text = $(this).text().trim(); if (text !== '/')
+        data.otherInfo.push(text); });
+    $(selector).find('.flex div:nth-child(2) > .mt-2.text-sm a').each(function () { data.search.push($(this).text().trim()); });
+    $(selector).find('.mt-3 > div:nth-child(2) > div.flex.items-center > span').each(function () { data.genres.push($(this).text().trim()); });
+    data.originalPulication = $(selector).find('.mt-3 > div:nth-child(2) > div:nth-child(3) span.font-bold.uppercase').text().trim() || null;
+    data.mParkUploadStatus = $(selector).find('.mt-3 > div:nth-child(2) > div:nth-child(3) span.font-bold.uppercase').text().trim() || null;
+    data.description = $(selector).find('.limit-html-p').text().trim() || null;
+    return data;
+}
+async function getTitleChapters(id) {
+    if (!id)
+        throw new Error('id is required');
+    const response = await client_1.client.get(`/title/${id}`);
+    const $ = cheerio.load(response.data);
+    const data = [];
+    $('.scrollable-panel .group > div').each(function () {
+        data.push({ id: $(this).find('a').attr('href') || null, chapter: $(this).find('> div.space-x-1 a').text().trim() || null, uploaded: $(this).find('time').attr('data-time') || null });
+    });
+    return data;
+}
 exports.default = {
     getPopularUpdates,
     fetchChapterImages,
+    getLatestReleases,
+    getMemberUploads,
+    getRandomMangas,
+    getYWeekList,
+    getMplistsWeekly,
+    getMplistsNewlyAdded,
+    getMplistsMostLikes,
+    searchComics,
+    getTitleInfo,
+    getTitleChapters,
 };
